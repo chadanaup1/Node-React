59de849ba0818b3181707df05dbd8f38df2839ba
Android <chandanapanchumarthi@gmail.com>
2022-05-20 00:16:04 +0200

    initial commit

--- tic tac toe/js/main.js
+++ tic tac toe/js/main.js
@@ -1,1 +1,32 @@
 console.log('TIC TAC TOE')
+
+/*
+
+let count = 0
+var count = 0
+const count = 0
+
+
+
+const a1 = document.querySelector('#a1')
+
+a1.addEventListener('click', function() {
+    console.log('click on A1')
+}
+)
+
+*/
+
+const elements = ['X', 'O']
+let current_player = 0
+
+const cells = document.querySelectorAll('.cell')
+
+cells.forEach(cell => {
+    cell.addEventListener('click', (e) =>  {
+          //  console.log(e)
+          //  e.target.style.background = 'red'
+          e.target.innerHTML = elements[current_player]
+    })
+})
+
